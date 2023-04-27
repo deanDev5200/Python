11da2c216b4fa71dfc73fc05d1eb00c335f7f291
Dean Putera <deanpop5200@gmail.com>
2023-04-27 19:15:09 +0800

    same

--- Testing/DDBot/test.py
+++ Testing/DDBot/test.py
@@ -1,117 +1,58 @@
-import nltk
-from nltk.stem.lancaster import LancasterStemmer
-import numpy as np
-import tflearn
-import tensorflow as tf
-import json
-import pickle
-import random
+from nltk.tokenize import word_tokenize
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+import wikipedia as wiki
+username = "Dean"
+wiki.set_lang('id')
+question_words = ["apa", "apakah", "siapa", "bagaimana", "kenapa", "kapan", "dimana", 
+              "mengapa", "pernahkah", 
+             "mana", "bisakah", "maukah", 
+             "haruskah", "punyakah", "berapa", "berapakah"]
 
-#Loading intents.json
-with open('intents.json') as intents:
-  data = json.load(intents)
 
-stemmer = LancasterStemmer()
+def stem(text:str):
+    fact = StemmerFactory()
+    stemmer = fact.create_stemmer()
+    return stemmer.stem(text)
 
-try:
-    with open('data.pickle','rb') as f:
-        words, labels, training, output = pickle.load(f)
-except:
-# Fetching and Feeding information--
-    words = []
-    labels = []
-    x_docs = []
-    y_docs = []
+def answer_question(question:str):
+    respond = ""
+    question = question.lower()
+    stemmed = stem(question)
+    print(stemmed)
+    tokenized = word_tokenize(question)
 
-    for intent in data['intents']:
-        for pattern in intent['patterns']:
-            wrds = nltk.word_tokenize(pattern)
-            words.extend(wrds)
-            x_docs.append(wrds)
-            y_docs.append(intent['tag'])
-
-            if intent['tag'] not in labels:
-                labels.append(intent['tag'])
-
-    words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
-    words = sorted(list(set(words)))
-    labels = sorted(labels)
-
-    training = []
-    output = []
-
-    out_empty = [0 for _ in range(len(labels))]
-
-    # One hot encoding, Converting the words to numerals
-    for x, doc in enumerate(x_docs):
-        bag = []
-        wrds = [stemmer.stem(w) for w in doc]
-        for w in words:
-            if w in wrds:
-                bag.append(1)
+    if any(x in tokenized[0] for x in question_words):
+        if tokenized[0] == question_words[0]:
+            if stemmed.find("adalah cerdas buat") != -1:
+                respond = "Benar sekali!"
+            elif stemmed.find("kabar") != -1:
+                respond = f"Aku sangat baik, terimakasih telah bertanya, bagaimana denganmu " + username
+        elif tokenized[0] == question_words[1]:
+            if stemmed.find("adalah cerdas buat") != -1:
+                respond = "Benar sekali!"
+            elif stemmed.find("kamu cerdas buat") != -1:
+                respond = "Benar sekali!"
+        elif tokenized[0] == question_words[2]:
+            print(tokenized[1])
+            if tokenized[1] == "namamu":
+                respond = f"Nama saya adalah"
+            elif tokenized[1] == "kamu":
+                respond = f"Namaku Dia sangat suka programming, Dia berasal dari Buleleng, Bali"
             else:
-                bag.append(0)
+                search_term = question.split(tokenized[0])[-1]
+                f = "Aku tidak menemukan apapun"
+                try:
+                    f = wiki.summary(search_term, sentences=5)
+                except wiki.exceptions.PageError:
+                    pass
+                respond = f
+        elif tokenized[0] == question_words[3]:
+            if stemmed.find("gempa kini") != -1:
+                respond = "hdskrthseghrd"
+        print(respond)
+        print("This is a question!")
+    else:
+        print("This is not a question!")
 
-
-        output_row = out_empty[:]
-        output_row[labels.index(y_docs[x])] = 1
-
-        training.append(bag)
-        output.append(output_row)
-
-
-    training = np.array(training)
-    output = np.array(output)
-
-    with open('data.pickle','wb') as f:
-        pickle.dump((words, labels, training, output), f)
-
-
-net = tflearn.input_data(shape=[None, len(training[0])])
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, len(output[0]), activation='softmax')
-net = tflearn.regression(net)
-
-model = tflearn.DNN(net)
-
-model.fit(training, output, n_epoch=100, batch_size=8, show_metric=True)
-model.save('model.tflearn')
-
-
-def bag_of_words(s, words):
-    bag = [0 for _ in range(len(words))]
-    s_words = nltk.word_tokenize(s)
-    s_words = [stemmer.stem(word.lower()) for word in s_words]
-    for se in s_words:
-        for i, w in enumerate(words):
-            if w == se:
-                bag[i] = 1
-
-    return np.array(bag)
-
-
-def chat():
-    print("The bot is ready to talk!!(Type 'quit' to exit)")
-    while True:
-        inp = input("\nYou: ").lower()
-        if inp == 'quit':
-            break
-
-    #Porbability of correct response 
-        results = model.predict([bag_of_words(inp, words)])
-
-    # Picking the greatest number from probability
-        results_index = np.argmax(results)
-
-        tag = labels[results_index]
-
-        print(tag)
-        for tg in data['intents']:
-
-            if tg['tag'] == tag:
-                responses = tg['responses']
-                print("Bot:" + random.choice(responses))
-
-
-chat()
+quest = input("Enter: ")
+answer_question(quest)
