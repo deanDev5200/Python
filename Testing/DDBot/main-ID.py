01cf90a4a68cc38fdad632f31ceb13ec136bf792
Dean Putera <deanpop5200@gmail.com>
2023-10-08 19:05:23 +0800

    yes

--- Testing/DDBot/main-ID.py
+++ Testing/DDBot/main-ID.py
@@ -103,8 +103,8 @@ def answer_question(question:str):
                     URL = "https://www.kompas.com/tren"
 
                     HEADERS = ({'User-Agent':
-                                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
-                                (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',\
+                                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
+                                 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36',\
                                 'Accept-Language': 'en-US, en;q=0.5'})
 
                     webpage = requests.get(URL, headers=HEADERS)
@@ -129,8 +129,8 @@ def answer_question(question:str):
                     URL = "https://poltracking.com/rilis-temuan-survei-nasional-poltracking-indonesia-tendensi-peta-politik-pilpres-2024/"
 
                     HEADERS = ({'User-Agent':
-                                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
-                                (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',\
+                                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
+                                 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36',\
                                 'Accept-Language': 'en-US, en;q=0.5'})
 
                     webpage = requests.get(URL, headers=HEADERS)
