dc9ad3c541dd6cffba78f5d81699ac5119949386
Dean Putera <deanpop5200@gmail.com>
2023-12-26 08:05:30 +0800

    edit

--- test2.py
+++
@@ -1,5 +0,0 @@
-from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-
-fact = StemmerFactory()
-stemmer = fact.create_stemmer()
-print(stemmer.stem("memecahkan masalahnya dan menerjunkan parasutnya"))
