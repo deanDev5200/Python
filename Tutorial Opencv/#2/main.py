d36123a0f6fb4aab074236035d37294d9c3cdc1b
Dean Putera <deanpop5200@gmail.com>
2023-12-27 11:17:27 +0800

    s

--- Tutorial Opencv/#2/main.py
+++ Tutorial Opencv/#2/main.py
@@ -1,14 +1,14 @@
-import cv2, os
-__path__ = __file__[0:len(__file__)-len(os.path.basename(__file__))]
+import cv2
 
-
-img = cv2.imread(__path__ + 'kangguru.jpg')
+img = cv2.imread('kangguru.jpg')
 img = cv2.resize(img, (500, 500))
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(img, (51, 51), 0)
+img = cv2.flip(img, 1)
+img = cv2.GaussianBlur(img, (11, 11), 0)
 
-cv2.imshow('Image', img)
-cv2.imshow('Gray', gray)
-cv2.imshow('Blur', blur)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow('image', img)
+cv2.imshow('gray', gray)
+
+cv2.imwrite('tes.jpg', gray)
 
 cv2.waitKey(0)
