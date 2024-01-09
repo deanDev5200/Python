36936a5821cc8fce07657152ed82c342526b1d65
Dean Putera <deanpop5200@gmail.com>
2024-01-09 16:46:59 +0800

    a

--- Testing/ST/test.py
+++ Testing/ST/test.py
@@ -7,9 +7,12 @@ import cv2
 
 model_path = 'D:\Python\Testing\ST\gesture_recognizer.task'
 video = cv2.VideoCapture(0)
+_, view = video.read()
 def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
+    global view
     if len(result.gestures) > 0:
-        print(f'gesture recognition result: {result.gestures[0]}')
+        print(f'gesture recognition result: {result.gestures[0]}')  
+    view = output_image.numpy_view()
 
 options = GestureRecognizerOptions(
     base_options=BaseOptions(model_asset_path=model_path, delegate=BaseOptions.Delegate.CPU),
@@ -19,11 +22,15 @@ print('a')
 
 while True:
     _, img = video.read()
-    with GestureRecognizer.create_from_options(options) as recognizer:
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=img)
-        recognizer.recognize_async(mp_image, round(time.time()-startTime))
+    view = img
+    try:
+        with GestureRecognizer.create_from_options(options) as recognizer:
+            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=img)
+            recognizer.recognize_async(mp_image, round(time.time()-startTime))
+    except Exception as e:
+        print(e)
 
-    cv2.imshow("epep", mp_image.numpy_view())
+    cv2.imshow("epep", view)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     
