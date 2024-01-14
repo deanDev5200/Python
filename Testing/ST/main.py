d04ae536a34d0e4d07615577c1ffefee3da9fff7
Dean Putera <deanpop5200@gmail.com>
2024-01-14 13:17:36 +0800

    nice

--- Testing/ST/main.py
+++ Testing/ST/main.py
@@ -1,54 +1,41 @@
 import time
-import keyboard
 startTime = time.time()
-from mediapipe.tasks.python import BaseOptions
-from mediapipe.tasks.python.vision import GestureRecognizer, GestureRecognizerResult, GestureRecognizerOptions, RunningMode as VisionRunningMode
-import mediapipe as mp
-import cv2
+import torch, cv2
+print('torch %s %s' % (torch.__version__, torch.cuda.get_device_properties(0) if torch.cuda.is_available() else 'CPU'))
+from ultralytics import YOLO
+model = YOLO('best.pt')
+import keyboard
 
 model_path = 'D:\Python\Testing\ST\gesture_recognizer.task' 
 video = cv2.VideoCapture(0)
-_, view = video.read()
 scene = False
 
-def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
-    global view, jutsu, scene
-    view = output_image.numpy_view()
-    if len(result.gestures) > 0:
-        cat = result.gestures[0][0].category_name
-        view = cv2.putText(output_image.numpy_view().copy(), cat, (160, 360), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 6)
-        view = cv2.putText(view, cat, (160, 360), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
-        if cat == 'kagebunshin':
-            if not scene:
-                keyboard.press("Ctrl+Home")
-                time.sleep(0.1)
-                keyboard.release("Ctrl+Home")
-                scene = True
-            else:
-                keyboard.press("Ctrl+]")
-                time.sleep(0.1)
-                keyboard.release("Ctrl+]")
-                scene = False
-                
-            time.sleep(1.5)
-
-options = GestureRecognizerOptions(
-    base_options=BaseOptions(model_asset_path=model_path, delegate=BaseOptions.Delegate.CPU),
-    running_mode=VisionRunningMode.LIVE_STREAM,
-    result_callback=print_result)
-
 while True:
+    num = 0
     _, img = video.read()
-    #view = img
-    try:
-        with GestureRecognizer.create_from_options(options) as recognizer:
-            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=img)
-            recognizer.recognize_async(mp_image, round(time.time()-startTime))
-    except Exception as e:
-        print(e)
+
+    results = model.predict(img, classes=1, stream=True)
+    for r in results:
+        num = len(r.boxes.cls.tolist())
+        view = cv2.putText(r.orig_img, str(r.boxes.xywh.tolist()), (160, 360), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 6)
+        view = cv2.putText(view, str(r.boxes.xywh.tolist()), (160, 360), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+        if len(r.boxes.xyxy.tolist())>0:
+            view = cv2.rectangle(view, (int(r.boxes.xyxy.tolist()[0][0]), int(r.boxes.xyxy.tolist()[0][1])), (int(r.boxes.xyxy.tolist()[0][2]), int(r.boxes.xyxy.tolist()[0][3])), (255, 0, 0), 5)
 
     cv2.imshow("epep", view)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    if num > 0:
+        if not scene:
+            keyboard.press("Ctrl+Home")
+            time.sleep(0.5)
+            keyboard.release("Ctrl+Home")
+            scene = True
+        else:
+            keyboard.press("Ctrl+]")
+            time.sleep(0.5)
+            keyboard.release("Ctrl+]")
+            scene = False
+
 
 video.release()
--- Testing/ST/test.py
+++ Testing/ST/test.py
@@ -1,6 +1,18 @@
-import torch
+import torch, cv2, time
 print('torch %s %s' % (torch.__version__, torch.cuda.get_device_properties(0) if torch.cuda.is_available() else 'CPU'))
 from ultralytics import YOLO
-
 model = YOLO('best.pt')
-model('')
+cap = cv2.VideoCapture(0)
+
+while True:
+    num = 0
+    _, frame = cap.read()
+    results = model.predict(frame, classes=1, stream=True)
+    time.sleep(0.1)
+    for r in results:
+        num = len(r.boxes.cls)
+    if len(r.boxes.cls) > 0:
+        break
+
+
+cap.release()
